240c68980454fe0aacd2e4a17ccad815b6415b5c
Aditya1092 <91484084+Aditya1092@users.noreply.github.com>
2023-06-22 04:15:10 +0530

    last

--- js/app.js
+++ js/app.js
@@ -21,6 +21,20 @@ function loadbody() {
     spanWeekday.innerText = day
 }
 
+function toggle(){
+    let password = document.getElementById("password");
+    let eye = document.getElementById("toggle");
+
+    if(password.getAttribute("type") == "password"){
+        password.setAttribute("type","text");
+        eye.style.color = "#0be881";
+    }
+    else{
+        password.setAttribute("type","password");
+        eye.style.color = "#808080";
+    }
+}
+
 
 // Signup script
 
--- js/login.js
+++ js/login.js
@@ -15,3 +15,19 @@ loginForm.addEventListener('submit', e => {
         loginError.innerText = err.message;
     })
 })
+
+function togglePasswordVisibility() {
+    const passwordInput = document.getElementById("login-password");
+    const toggleIcon = document.getElementById("toggle-icon");
+  
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      toggleIcon.classList.remove("fa-eye");
+      toggleIcon.classList.add("fa-eye-slash");
+    } else {
+      passwordInput.type = "password";
+      toggleIcon.classList.remove("fa-eye-slash");
+      toggleIcon.classList.add("fa-eye");
+    }
+  }
+  
